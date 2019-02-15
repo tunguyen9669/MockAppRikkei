39d2bb5d08f14a529f9843a8f3027a0c2012e862
Nguyen Tu <tunguyen090896@gmail.com>
2019-02-15 13:39:22 +0700

    first commit

---
+++ MockApp/MockApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MockApp
+//
+//  Created by tund on 2/15/19.
+//  Copyright © 2019 tund. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
